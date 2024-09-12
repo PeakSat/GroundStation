b976367c4bc044a1d345d5b7666b7c5dc6ef9902
andronikos <andronikostaslos@gmail.com>
2024-09-12 16:39:13 +0300

    add logger functionality according to comms-software

--- Lib/logger-main-c6abda2e/src/Logger.cpp
+++ Lib/logger-main-c6abda2e/src/Logger.cpp
@@ -1,15 +1,75 @@
-#include <Logger.hpp>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "UARTGatekeeperTask.hpp"
+#include "Logger.hpp"
 
-etl::format_spec Logger::format;
+#define MaxLogNameSize 9
+#define MaxTickCountStringSize 10
 
-Logger::LogEntry::LogEntry(LogLevel level) : level(level) {}
+void Logger::log(Logger::LogLevel level, etl::istring &message) {
+    etl::string<MaxLogNameSize> levelString;
+    etl::string<MaxTickCountStringSize> time;
 
-Logger::LogEntry::~LogEntry() {
-	// When the destructor is called, the log message is fully "designed". Now we can finally "display" it to the user.
-	Logger::log(level, message);
+    if (level <= Logger::trace) {
+        levelString.append("trace");
+    } else if (level <= Logger::debug) {
+        levelString.append("debug");
+    } else if (level <= Logger::info) {
+        levelString.append("info");
+    } else if (level <= Logger::notice) {
+        levelString.append("notice");
+    } else if (level <= Logger::warning) {
+        levelString.append("warning");
+    } else if (level <= Logger::error) {
+        levelString.append("error");
+    } else {
+        levelString = "emergency";
+    }
+
+    while (levelString.available()) {
+        levelString.append(" ");
+    }
+
+    etl::to_string(xTaskGetTickCount(), time, format.width(MaxTickCountStringSize), 0);
+
+    etl::string<LOGGER_MAX_MESSAGE_SIZE> output;
+    output.append(time.c_str());
+    output.append(" [");
+    output.append(levelString.c_str());
+    output.append("] ");
+
+    etl::string<MaxLogNameSize> subsystemString = "COMMS";
+    while (subsystemString.available()) {
+        subsystemString.append(" ");
+    }
+    output.append(subsystemString.c_str());
+
+    output.append(message.c_str());
+    output.append("\n");
+
+    if (1) {
+        if (uartGatekeeperTask) {
+            uartGatekeeperTask->addToQueue(output);
+        }
+    }
+
 }
 
-template<>
-void convertValueToString(String<LOGGER_MAX_MESSAGE_SIZE>& message, float value) {
-    etl::to_string(value, message, Logger::format.precision(Logger::MaxPrecision), true);
+
+// Reimplementation of the log function for C++ strings
+// This is kept in the Platform files, since we don't want to mess with std::strings in the microcontroller
+Logger::LogEntry &Logger::LogEntry::operator<<(const std::string &value) {
+    message.append(value.c_str());
+
+    return *this;
+}
+
+Logger::LogEntry& Logger::LogEntry::operator<<(char* value) {
+    message.append(value);
+    return *this;
+}
+
+Logger::LogEntry& Logger::LogEntry::operator<<(const char* value) {
+    message.append(value);
+    return *this;
 }
