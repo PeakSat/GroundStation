9df7412d5198e205751180d44a79eb2b6b353b8b
AndronikosKostas <andronikostaslos@gmail.com>
2025-01-23 12:38:31 +0200

    refactor: minor

--- App/app_main.cpp
+++ App/app_main.cpp
@@ -8,8 +8,6 @@
 #include "UARTGatekeeperTask.hpp"
 #include "RF_TXTask.hpp"
 #include "RF_RXTask.hpp"
-#include "at86rf215.hpp"
-
 
 
 void app_main( void )
@@ -22,8 +20,6 @@ void app_main( void )
     uartGatekeeperTask->createTask();
     rf_rxtask->createTask();
     rf_txtask->createTask();
-    HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-    HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 
     transceiver_handler.initialize_semaphore();
@@ -36,17 +32,7 @@ void app_main( void )
 }
 /*-----------------------------------------------------------*/
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-    HAL_GPIO_EXTI_IRQHandler(RF_IRQ_Pin);
-    transceiver.handle_irq();
-}
-
 extern "C" void EXTI15_10_IRQHandler(void) {
-    transceiver.handle_irq();
-}
-
-extern "C" [[maybe_unused]] void EXTI1_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(RF_IRQ_Pin);
     transceiver.handle_irq();
 }
